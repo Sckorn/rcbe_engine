e07f2b918c4903f5ec174b953d2899b72da65a73
sckorn <sckorn@gmail.com>
2018-05-02 19:27:50 +0300

    nlohmann json added, simple program options

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+#include <boost/program_options.hpp>
+
+
+
 int main(int argc, char * argv[])
 {
-  std::cout << "Fuck you!" << std::endl;
+  namespace po = boost::program_options;
+  po::options_description desc("Allowed options");
+  desc.add_options()
+        ("help,h", "produce help message")
+        ("config,c", po::value<std::string>(), "provide configuration file");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+  {
+    std::cout << desc << std::endl;
+    return 0;
+  }
+
   return 0;
 }
