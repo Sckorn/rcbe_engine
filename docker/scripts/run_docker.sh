2509b46e0a27c410dab2a7fca21396bc6619fbd8
Daniel Kiryeyev <sckorn@gmail.com>
2019-08-04 22:02:01 +0300

    Add docker (#12)
    * started adding docker file for our docker image
    
    * main.docker builds successfully
    
    * docker built successfully, clion doesn't load necessary plugins
    
    * removed unneeded files, remoed some unwanted code lines
    
    * add-docker switched to vscode, all done
    
    * review fixes

---
+++ docker/scripts/run_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export HUID="$(id -u)"
+export HGID="$(id -g)"
+export OPER_HOME="/home/oper"
+export DISPLAY
+
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+xhost +local:
+
+docker run -it --rm -e DISPLAY=unix$DISPLAY -e HUID=${HUID} -e HGID=${HGID} -e OPER_HOME=${OPER_HOME} -v /tmp/.X11-unix:/tmp/.X11-unix -v ${DIR}/../../:/rcbe_engine/ -v /dev/snd:/dev/snd --privileged --network host --entrypoint /entrypoint.sh $1 /bin/bash
+
