5aa611ccc676e0c18ae35b9e2e237708dde82845
Daniel Kiryeyev <sckorn@gmail.com>
2020-11-29 16:59:17 +0300

    Fix deploy script and add deploy to CI. Trigger deploy upon release creation. (#51)

--- deploy_packages.py
+++ deploy_packages.py
@@ -3,14 +3,20 @@
 import sys
 import os
 import shutil
+import argparse
 from shutil import copyfile
 
-## TODO: add proper command line args prase and usage
-def usage():
-    print("deploy_packages.py")
-    print("A simple python script for engine deb packages build and deployment.")
-    print('Usage:')
-    print('python3 ./deploy_packages.py root/directory/with/built/packages [bazel-bin] required-engine-version [0.0.3] target-architecture [amd64] resulting-tar-path [/tmp/deploy.tar.gz]')
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser(description="RCBE deb packages deploy script")
+    parser.add_argument('-d', '--packages-directory', default='bazel-bin', help='Path to directory with built deb pacakges')
+    parser.add_argument('-e', '--engine-version', default='0.0.3', help='Engine version for which deb packages were built')
+    parser.add_argument('-a', '--target-architecture', default='amd64', help='Engine target architecture for which deb pacakges were built')
+    parser.add_argument('-o', '--output-directory', default='/tmp', help='Output directory where a tar.gz file containing all deployed deb pacakges will be created')
+    parser.add_argument('-p', '--packages-extension', default='deb', help='Extension of packages to be deployed')
+
+    parsed_args = parser.parse_args(argv)
+
+    return parsed_args.packages_directory, parsed_args.engine_version, parsed_args.target_architecture, parsed_args.output_directory, parsed_args.packages_extension
 
 def clear_dir(path):
     for filename in os.listdir(path):
@@ -36,17 +42,15 @@ def clear_and_rm_dir(tmp_dir):
     else:
         print ("Successfully deleted the directory %s " % tmp_dir)
 
-def scan_dir_for_deb(folder, engine_version, target_arch, extensions = ("deb")):
+def scan_dir_for_packages(folder, engine_version, target_arch, extensions = ("deb")):
     return [os.path.join(r, fn)
             for r, ds, fs in os.walk(folder)
             for fn in fs if fn.endswith(extensions) and fn.rfind(engine_version) != -1 and fn.rfind(target_arch) != -1]
 
-def main():
+def main(packages_path, engine_version, target_arch, output_file_path, extension):
     cwd = os.getcwd()
     print("CWD: {}".format(cwd))
-    deb_files_path = os.path.join(cwd, str(sys.argv[1]))
-    engine_version = sys.argv[2]
-    target_arch = sys.argv[3]
+    deb_files_path = os.path.join(cwd, str(packages_path))
     print("Trying to deploy version {}".format(engine_version))
     print("For arch {}".format(target_arch))
     print("Starting bazel build for deploy")
@@ -55,11 +59,11 @@ def main():
 
     if (bazel_exit_code != 0):
         print("Error while building packages")
-        exit(1);
+        exit(1)
 
     print("Searching in {}".format(deb_files_path))
 
-    found_deb_files = scan_dir_for_deb(deb_files_path, engine_version, target_arch)
+    found_deb_files = scan_dir_for_packages(deb_files_path, engine_version, target_arch, extension)
 
     print("Total found deb files {}".format(found_deb_files))
 
@@ -83,7 +87,8 @@ def main():
         copyfile(fp, full_new_name)
 
     print("Packing the temp directory into tar.gz")
-    output_file_path = sys.argv[4]
+
+    output_file_path = os.path.join(output_file_path, "rcbe_engine_{}_{}.tar.gz".format(engine_version, target_arch))
 
     tar_ec = os.system('tar -czvf {} -C {} .'.format(output_file_path, tmp_dir))
     if tar_ec != 0:
@@ -100,7 +105,6 @@ def main():
     print("Deploy finished!")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        usage()
-        exit(1)
-    main()
+    source_dir, engine_version, target_arch, output_file, extension = parse_arguments(sys.argv[1:])
+
+    main(source_dir, engine_version, target_arch, output_file, extension)
